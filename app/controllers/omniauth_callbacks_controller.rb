c90ca7a192d4d17e93a7ff1d0ca1b86d9a171a34
clindsay107 <clindsay107@gmail.com>
2016-11-06 18:51:42 -0800

    reenable mail auth check

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -20,7 +20,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def after_sign_in_path_for(resource)
-    if resource.confirmed?
+    if resource.email_verified?
       super resource
     else
       finish_signup_path(resource)
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -10,22 +10,25 @@ class Profile < ApplicationRecord
 
   def find_match
     if self.candidate = 'Hillary Clinton'
-      m_id = Profile.where(
+      m_profile = Profile.where(
         candidate: ['Jill Stein', 'Gary Johnson', 'Evan McMullin'],
         state: swing_states,
         match_id: nil
       ).sample
 
-      if m_id.nil?
+      if m_profile.nil?
         return
       end
 
-      m = User.where(id: m_id.id)
-      if m.empty?
+      m_profile.match_id = self.id
+      m_profile.save
+
+      m_user = User.where(id: m_profile.id)
+      if m_user.empty?
         return
       end
 
-      self.match_id = m.first.id
+      self.match_id = m_user.first.id
       self.save
     else
       #
