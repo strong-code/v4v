59cea92d119ed73a3fba7dc4d5bf978d307a96bb
clindsay107 <clindsay107@gmail.com>
2016-11-06 19:02:55 -0800

    typo domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'vote4vote.heroku.com',
+  :domain         => 'vote4vote.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
